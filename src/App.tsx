309ea1cc754aab811e5120adb0eaf5a648807f54
abiya005 <abiyabiju050805@gmail.com>
2025-01-12 20:23:24 +0530

    tsx file added code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import Alert from "./components/Alert";
+import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
   return (
     <div>
-      <Alert text="Hello World" />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
